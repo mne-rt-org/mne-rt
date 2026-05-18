a232f3ba62ad7645f950178f41fb165b7bb562a5
payam <payam.sadeghi74@gmail.com>
2026-05-18 12:06:21 +0200

    artifact detectors (riemann + bad channel detector) and simulation modules added

--- src/ant/tools/__init__.py
+++ src/ant/tools/__init__.py
@@ -1,7 +1,11 @@
 from .tools import *
 from .tools import _compute_inv_operator
-from .simulation import simulate_raw, simulate_eeg_raw
+from .simulation import simulate_raw, simulate_nf_session
+from .lms import AdaptiveLMSFilter
 from .orica import ORICA
 from .gedai import GEDAIDenoiser
 from .asr import ASRDenoiser
 from .maxwell import RTMaxwellFilter
+from .bad_channel_detector import BadChannelDetector
+from .riemannian_potato import RiemannianPotatoDetector
+from .bids_io import save_as_bids
